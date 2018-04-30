92f5ef2c24224a7057556eb2972d442ca66cbd95
Yun <dbsdudguq@gmail.com>
2018-05-01 01:31:58 +0900

    ExportXML code improve.

--- src/main/kotlin/Properties.kt
+++ src/main/kotlin/Properties.kt
@@ -1,3 +1,16 @@
+import javax.xml.transform.OutputKeys
+
 object Properties {
     const val APP_TITLE = "Xml Converter"
+
+    object XmlPropertyKeys {
+        const val INDENT_AMOUNT = "{http://xml.apache.org/xslt}indent-amount"
+        const val INDENT = OutputKeys.INDENT
+        const val OMIT_XML_DECLARATION = OutputKeys.OMIT_XML_DECLARATION;
+    }
+
+    object XmlPropertyValue {
+        const val YES = "yes"
+        const val INDENT_VAULE: String = "4"
+    }
 }
