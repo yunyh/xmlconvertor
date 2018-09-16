ef282e6b926b9af1dd9b3634a35b18d87a9461ca
mark.yun <dbsdudguq@gmail.com>
2018-09-16 22:37:24 +0900

    gradle version up. kotlin build script fix.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,105 +1,20 @@
-import org.gradle.api.file.SourceDirectorySet
-import org.gradle.api.internal.HasConvention
-import org.gradle.api.tasks.SourceSet
-import org.gradle.api.tasks.SourceSetContainer
-import org.gradle.jvm.tasks.Jar
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jetbrains.kotlin.js.translate.context.Namer.kotlin
 
-version = "1.0"
-
-buildscript {
-    var kotlinVersion: String by extra
-    kotlinVersion = "1.2.41"
-
-    repositories {
-        mavenCentral()
-        jcenter()
-    }
-
-    dependencies {
-        classpath(kotlinModule("gradle-plugin", kotlinVersion))
-    }
+plugins {
+    kotlin("jvm") version "1.2.70"
 }
 
-apply {
-    plugin("kotlin")
-    plugin("maven")
-}
-
-val kotlinVersion: String by extra
+group = "my"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    compile(kotlinModule("stdlib-jdk8", kotlinVersion))
+    compile(kotlin("stdlib-jdk8"))
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
-}
-
-/*
-val sourceJar = task<Jar>("sourceJar") {
-    classifier = "sources"
-    from(the<JavaPluginConvention>().sourceSets.getByName("main").allSource)
-}
-
-artifacts.add("archives", sourceJar)
-*/
-
-/*
-application {
-    mainClassName = "MainKt"
-}
-
-tasks.withType<Jar>{
-    baseName = "MyApp"
-    version = "0.0.1"
-
-    configurations["compileClasspath"].forEach { file: File ->
-        from(zipTree(file.absoluteFile))
-    }
-
-    manifest {
-        attributes["Main-Class"] = "MainKt"
-    }
-}
-*/
-
-/*
-val fatJar = task("fatJar", type = Jar::class) {
-    baseName = "${project.name}-fat"
-    manifest {
-        attributes["Main-Class"] = "MainKt"
-    }
-    from(configurations.runtime.map({ if (it.isDirectory) it else zipTree(it) }))
-    with(tasks["jar"] as CopySpec)
-}
-
-tasks {
-    "build" {
-        dependsOn(fatJar)
-    }
-}
-*/
-
-val fatJar = task("fatJar", type = Jar::class) {
-    baseName = "XmlConverter"
-    manifest {
-        attributes["Implementation-Title"] = "My App"
-        attributes["Implementation-Version"] = version
-        attributes["Main-Class"] = "MainKt"
-    }
-    from(configurations.runtime.map({ if (it.isDirectory) it else zipTree(it) }))
-    with(tasks["jar"] as CopySpec)
-}
-
-tasks {
-    "jar" {
-        dependsOn(fatJar)
-    }
 }
