9d5ab9011360eec6ecffa42684dfe1fd157b124d
yunyh <dbsdudguq@gmail.com>
2020-08-27 12:20:41 +0900

    build error fix.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,32 +9,26 @@ plugins {
 
     kotlin("jvm") version kotlinVersion
     id("org.openjfx.javafxplugin") version openjfxVersion
-
-
 }
 
 repositories {
     mavenCentral()
     jcenter()
-
 }
 
 dependencies {
-
     val coroutineVersion = "1.3.9"
 
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-javafx:$coroutineVersion")
 }
+
 tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+    kotlinOptions.jvmTarget = "1.8"
 }
 
 javafx {
     version = "14"
     modules("javafx.controls", "javafx.fxml")
-    configuration = "compileOnly"
 }
